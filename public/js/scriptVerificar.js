7e6e202da3b65b4ea4a9f1e51bc499c100007363
GuinhoFSilva <guilherme.silva@sptech.school>
2024-11-14 11:32:23 -0300

    Realizando ajustes no status logado

--- public/js/scriptVerificar.js
+++ public/js/scriptVerificar.js
@@ -1,26 +1,29 @@
+var loginFeito = sessionStorage.NOME_USUARIO
 window.onload = function() {
     verificaLogin();
-    sair();
-
+    // sair()
 }
 function verificaLogin() {
-    if(sessionStorage.NOME_USUARIO != 'undefined') {
+    if(loginFeito != undefined) {
         document.getElementById('loginCadastro').style.display = 'none';
         document.getElementById('botaoSair').style.display = 'block'
-     }else{
-        sair()
+    }else{
+        document.getElementById('loginCadastro').style.display = 'block';
+         document.getElementById('botaoSair').style.display = 'none'
+
      }
 }
 
 function sair(){
-    if(sessionStorage.NOME_USUARIO = undefined) {
-        sessionStorage.removeItem('NOME_USUARIO');
-        sessionStorage.removeItem('EMAIL_USUARIO');
-        sessionStorage.removeItem('ID_USUARIO');
+    if(loginFeito != undefined) {
+        loginFeito == undefined
         setTimeout (() => {
             window.location.href = "./index.html";
-            document.getElementById('botaoSair').style.display = 'none'
             document.getElementById('loginCadastro').style.display = 'block';
+            document.getElementById('botaoSair').style.display = 'none'
         }, 2000);
-     }
+            sessionStorage.removeItem('NOME_USUARIO');
+            sessionStorage.removeItem('EMAIL_USUARIO');
+            sessionStorage.removeItem('ID_USUARIO');
+        }
 }
