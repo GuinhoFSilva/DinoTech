583b7c773ee458a497cf35a17beedf27a92374a8
GuinhoFSilva <guilherme.silva@sptech.school>
2024-11-24 12:06:18 -0300

    finalizando o ranking e ajustando erros

--- public/js/scriptVerificar.js
+++ public/js/scriptVerificar.js
@@ -1,13 +1,13 @@
-var loginFeito = sessionStorage.NOME_USUARIO
 window.onload = function() {
     verificaLogin();
     // sair()
 }
 function verificaLogin() {
+    var loginFeito = sessionStorage.NOME_USUARIO
     if(loginFeito != undefined) {
         document.getElementById('divquiz').style.display = 'block';
-        document.getElementById('botaoSair').style.display = 'block'
         document.getElementById('divleader').style.display = 'block'
+        document.getElementById('botaoSair').style.display = 'block'
         document.getElementById('divlogin').style.display = 'none';
         document.getElementById('divcadastro').style.display = 'none';
     }else{
@@ -15,14 +15,15 @@ function verificaLogin() {
         document.getElementById('divlogin').style.display = 'block';
         document.getElementById('divquiz').style.display = 'none';
         document.getElementById('divleader').style.display = 'none';
-         document.getElementById('botaoSair').style.display = 'none'
+        document.getElementById('botaoSair').style.display = 'none'
 
      }
 }
 
 function sair(){
+    var loginFeito = sessionStorage.NOME_USUARIO
     if(loginFeito != undefined) {
-        loginFeito == undefined
+        loginFeito = undefined
         setTimeout (() => {
             window.location.href = "./index.html";
             document.getElementById('divlogin').style.display = 'block';
